14e31cf8b2acdf02417a55bf35ec51abfe7f99c7
p-mano <manoj.pasupuletik@gmail.com>
2019-06-05 17:08:01 +0530

    practice reactj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
+import Home from './component/home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      headerText: "Header",
+    }
+  }
+
+    printIt(){
+      alert('i am passed here')
+    }
+
+    onChangeHeader(newName){
+      this.setState({
+        headerText:newName
+      })
+
+    }
+
+  render() {
+    let user={
+      name:'manoj kumar',
+      hobbies:['Cricket','football','Story Writing']
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+         
+          <p><Header  headerText={this.state.headerText}  /></p>
+
+        </header>
+
+        <p><Home 
+             name={'mano'} 
+             age={27} 
+             user={user}  
+             print={this.printIt}  
+             changeText={this.onChangeHeader.bind(this)}
+             initialName={this.state.headerText}
+             
+             /></p>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/component/home.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    constructor(props) {
+        super();//excute the parent component
+        this.state = {
+            age: props.age,
+            headerText:props.initialName,
+           
+        }
+    }
+
+    increaseCount() {
+        this.setState({
+            age: this.state.age + 2,
+        })
+
+    }
+    onChangeText(){
+        this.props.changeText(this.state.headerText)
+    }
+
+    onHandleChange(event){
+        this.setState({
+           headerText:event.target.value
+        })
+    }
+
+    render() {
+
+        let text = 'hello react! ';
+        return (
+            <div>
+                {text}
+                <p>Your Name is : {this.props.name}</p>
+                <p>Your Age is : {this.state.age}</p>
+                <p>user name: {this.props.user.name}</p>
+                <ul>
+
+                    {this.props.user.hobbies.map((hobb, i) => <li key={i}>{hobb}</li>)}
+
+                </ul>
+                <hr />
+                <button onClick={() => this.increaseCount()} className="btn btn-success">On Click Increase Count</button>
+                <hr />
+                <button onClick={this.props.print} className="btn btn-primary"> Child to Parent</button>
+                <hr/>
+                <input  type='tetx' value={this.state.initialName} onChange={(event)=>this.onHandleChange(event)} />
+                <button onClick={this.onChangeText.bind(this)} className="btn btn-primary">change Header</button>
+
+            </div>
+        )
+    }
+
+}
+
+export default Home;
